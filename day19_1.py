1f58b739ceffa3cd83acd40464981f973705da94
Patrick Stover <stoverp@colorado.edu>
2023-01-10 15:11:24 -0700

    day 19 non-recursive, no pruning

--- day19_1.py
+++ day19_1.py
@@ -1,6 +1,7 @@
 import re
 import time
 from argparse import ArgumentParser
+from dataclasses import dataclass, asdict, astuple
 from enum import Enum
 
 
@@ -10,173 +11,150 @@ class Resource(Enum):
   OBSIDIAN = 2
   GEODE = 3
 
-
-class Global(object):
-  total_branches = 0
-  cache = dict()
-  cache_hits = 0
-  cache_misses = 0
-
-  @staticmethod
-  def reset():
-    Global.total_branches = 0
-    Global.cache = dict()
-    Global.cache_hits = 0
-    Global.cache_misses = 0
-
-
-class Robot(object):
-  def __init__(self, type, cost):
-    self.type = type
-    self.cost = cost
+  def __str__(self):
+    return self.name.lower()
 
   def __repr__(self):
-    return f"Robot(type={self.type}, cost={self.format_cost()}"
+    return str(self)
+
+
+@dataclass(frozen=True)
+class MaterialSet:
+  ore: int = 0
+  clay: int = 0
+  obsidian: int = 0
+  geode: int = 0
+
+  def __add__(self, other):
+    return MaterialSet(*[a + b for a, b in zip(astuple(self), astuple(other))])
+
+  def __sub__(self, other):
+    return MaterialSet(*[a - b for a, b in zip(astuple(self), astuple(other))])
+
+  def __le__(self, other):
+    return all(a <= b for a, b in zip(astuple(self), astuple(other)))
+
+  def __getitem__(self, item):
+    return getattr(self, str(item))
 
   def format_cost(self):
-    return " and ".join(f"{amount} {Resource(index).name.lower()}" for index, amount in enumerate(self.cost) if amount > 0)
+    return " and ".join(f"{amount} {attr}" for attr, amount in asdict(self).items() if amount > 0)
+
+  @classmethod
+  def from_dict(cls, d):
+    return MaterialSet(
+      d.get(Resource.ORE, 0),
+      d.get(Resource.CLAY, 0),
+      d.get(Resource.OBSIDIAN, 0),
+      d.get(Resource.GEODE, 0)
+    )
 
 
-def buildable_robot_types(blueprint, collection):
-  # can choose to not build a robot
-  result = [None]
-  for resource in Resource:
-    can_build = True
-    for cost_resource_index, cost_value in enumerate(blueprint[resource].cost):
-      if collection[cost_resource_index] < cost_value:
-        can_build = False
-    if can_build:
-      result.append(resource)
-  return result
+@dataclass(frozen=True)
+class State:
+  materials: MaterialSet
+  robots: MaterialSet
+  built_robot_type: Resource
+  minutes_remaining: int
+  parent: 'State'
 
 
-def update_cache(robots, collection, minutes_remaining, choices, collected):
-  Global.cache[(robots, collection, minutes_remaining)] = (choices, collected)
-
-
-def read_cache(robots, collection, minutes_remaining):
-  key = (robots, collection, minutes_remaining)
-  if key in Global.cache:
-    return Global.cache[key]
-  return None
-
-
-def merge(collected1, collected2):
-  total = dict()
-  for resource in collected1.keys():
-    total[resource] = collected1.get(resource, 0) + collected2.get(resource, 0)
-  return total
-
-
-def greater_than(collected1, collected2):
-  for resource in reversed(Resource):
-    if collected1[resource.value] > collected2[resource.value]:
-      return True
-    elif collected1[resource.value] < collected2[resource.value]:
-      return False
-  return False
-
-
-def update_amounts(amounts, resource, value):
-  return tuple(amount + (value if index == resource.value else 0) for index, amount in enumerate(amounts))
-
-
-def add_amounts(amounts1, amounts2, factor=1):
-  return tuple(a1 + (a2 * factor) for a1, a2 in zip(amounts1, amounts2))
-
-
-def subtract_amounts(amounts1, amounts2):
-  return add_amounts(amounts1, amounts2, factor=-1)
-
-
-def run(blueprint, robots, collection, total_minutes, minutes_remaining):
-  if minutes_remaining == 0:
-    Global.total_branches += 1
-    if Global.total_branches % 1000000 == 0:
-      print(f"branches so far: {Global.total_branches}")
-    return [], collection
-  cache_result = read_cache(robots, collection, minutes_remaining)
-  if cache_result:
-    Global.cache_hits += 1
-    return cache_result
-  Global.cache_misses += 1
-  buildable_types = buildable_robot_types(blueprint, collection)
-  best_choices = []
-  best_collected = init_resource_amounts()
-  collection = add_amounts(collection, robots)
-  for build_resource_type in buildable_types:
-    if build_resource_type:
-      robots = update_amounts(robots, build_resource_type, 1)
-      collection = subtract_amounts(collection, blueprint[build_resource_type].cost)
-    choices, collected = run(blueprint, robots, collection, total_minutes, minutes_remaining - 1)
-    if greater_than(collected, best_collected):
-      best_choices = [build_resource_type] + choices
-      best_collected = collected
-  update_cache(robots, collection, minutes_remaining, best_choices, best_collected)
-  return best_choices, best_collected
-
-
-def init_resource_amounts():
-  return 0, 0, 0, 0
-
-
-def print_choices(choices, blueprint, minutes_remaining):
-  robots = [1, 0, 0, 0]
-  collection = [0, 0, 0, 0]
-  for build_robot_type, minute in zip(choices, range(minutes_remaining)):
-    print(f"\n== Minute {minute + 1} ==")
-    if build_robot_type:
-      for resource_type, amount in enumerate(blueprint[build_robot_type].cost):
-        collection[resource_type] -= amount
-      if build_robot_type == Resource.GEODE:
-        print(f"Spend {blueprint[Resource.GEODE].format_cost()} to start building a geode-cracking robot.")
-      else:
-        print(f"Spend {blueprint[build_robot_type].format_cost()} to start building a {build_robot_type.name.lower()}-collecting robot.")
-    for resource_type, n_robots in enumerate(robots):
-      if n_robots == 0:
-        continue
-      collection[resource_type] += n_robots
-      resource = Resource(resource_type)
-      if resource_type == Resource.GEODE:
-        print(f"{n_robots} geode-cracking robot{'s' if n_robots > 1 else ''} crack{'s' if n_robots == 1 else ''} geodes; you now have {collection[Resource.GEODE.value]} open geodes.")
-      else:
-        print(f"{n_robots} {resource.name.lower()}-collecting robot{'s' if n_robots > 1 else ''} collect{'s' if n_robots == 1 else ''} {resource.name.lower()}; you now have {collection[resource_type]} {resource.name.lower()}.")
-    if build_robot_type:
-      robots[build_robot_type.value] += 1
-      print(f"The new {build_robot_type.name.lower()}-collecting robot is ready; you now have {robots[build_robot_type.value]} of them.")
-
-
-def main(file, minutes):
+def read_blueprints(file):
+  blueprints = dict()
   with open(file, "r") as f:
-    Global.reset()
-    blueprint = dict()
     for line in f:
       if match := re.match(f"Blueprint (\d+):", line):
         blueprint_id = int(match.group(1))
       else:
         raise Exception(f"invalid input line: {line}")
-      if matches := re.findall(r"(Each (\w+) robot costs (\d+) (\w+)( and (\d+) (\w+))?.)", line):
+      if matches := re.findall(r"Each (\w+) robot costs (.*?)\.", line):
+        blueprint = dict()
         for match in matches:
-          robot_type = Resource[match[1].upper()]
-          cost = update_amounts(init_resource_amounts(), Resource[match[3].upper()], int(match[2]))
-          if len(match[4]) > 0:
-            cost = update_amounts(cost, Resource[match[6].upper()], int(match[5]))
-          blueprint[robot_type] = Robot(robot_type, cost)
+          robot_type = Resource[match[0].upper()]
+          materials_dict = dict()
+          for resource_cost in match[1].split(" and "):
+            amount, type = resource_cost.split(" ")
+            materials_dict[Resource[type.upper()]] = int(amount)
+          materials = MaterialSet.from_dict(materials_dict)
+          blueprint[robot_type] = materials
       else:
         raise Exception(f"invalid input line: {line}")
-      print(f"\n\n*** Blueprint {blueprint_id} ***")
-      for resource in Resource:
-        print(f"  Each {resource.name.lower()} robot costs {blueprint[resource].format_cost()}.")
-      robots = (1, 0, 0, 0)
-      collection = init_resource_amounts()
-      choices, collected = run(blueprint, robots, collection, minutes, minutes)
-      print_choices(choices, blueprint, minutes)
-      print(f"\nchoices: {choices}")
-      print(f"final collection: {collected}")
-      print(f"cache hits: {Global.cache_hits}")
-      print(f"cache misses: {Global.cache_misses}")
-      print(f"total branches examined: {Global.total_branches}")
-      return
+      blueprints[blueprint_id] = blueprint
+  return blueprints
+
+
+def print_blueprint(id, blueprint):
+  print(f"\n\n*** Blueprint {id} ***")
+  for resource in Resource:
+    print(f"  Each {resource.name.lower()} robot costs {blueprint[resource].format_cost()}.")
+
+
+def find_max_geodes(blueprint, minutes):
+  best_state = None
+  initial_state = State(MaterialSet(), MaterialSet(ore=1), None, minutes, None)
+  queue = [initial_state]
+  while queue:
+    state = queue.pop(0)
+    minutes_remaining = state.minutes_remaining - 1
+    if minutes_remaining == 0:
+      # if best_state and state.materials.geode > best_state.materials.geode:
+      if not best_state or state.materials.obsidian > best_state.materials.obsidian:
+        best_state = state
+    else:
+      # consider state with no robots built
+      queue.append(State(
+        state.materials + state.robots,
+        state.robots,
+        None,
+        minutes_remaining,
+        state
+      ))
+      # consider states with each type of robot built (if affordable)
+      for robot_type in Resource:
+        if blueprint[robot_type] <= state.materials:
+          queue.append(State(
+            state.materials + state.robots - blueprint[robot_type],
+            state.robots + MaterialSet.from_dict({robot_type: 1}),
+            robot_type,
+            minutes_remaining,
+            state
+          ))
+  return best_state
+
+
+def path(final_state):
+  path = []
+  state = final_state
+  while state:
+    path.append(state)
+    state = state.parent
+  return list(reversed(path))
+
+
+def main(file, minutes):
+  blueprints = read_blueprints(file)
+  # DEBUG: first blueprint only
+  blueprint = blueprints[1]
+  print_blueprint(1, blueprint)
+  final_state = find_max_geodes(blueprint, minutes)
+  state_path = path(final_state)
+  minute = 1
+  materials = MaterialSet()
+  robots = MaterialSet(ore=1)
+  for state in state_path:
+    print(f"== Minute {minute} ==")
+    if state.built_robot_type:
+      materials = materials - blueprint[state.built_robot_type]
+      print(f"Spend {blueprint[state.built_robot_type].format_cost()} to start building a {state.built_robot_type.name.lower()}-collecting robot.")
+    materials = materials + robots
+    for resource, amount in asdict(robots).items():
+      if amount == 0:
+        continue
+      print(f"{amount} {resource}-collecting robot{'s' if amount > 1 else ''} collect{'' if amount > 1 else 's'} {amount} {resource}; you now have {materials[resource]} {resource}")
+    if state.built_robot_type:
+      robots = robots + MaterialSet.from_dict({state.built_robot_type: 1})
+      print(f"The new {state.built_robot_type}-collecting robot is ready; you now have {robots[state.built_robot_type]} of them.")
+    minute += 1
 
 
 if __name__ == "__main__":
---
+++ day19_1_old.py
@@ -0,0 +1,189 @@
+import re
+import time
+from argparse import ArgumentParser
+from enum import Enum
+
+
+class Resource(Enum):
+  ORE = 0
+  CLAY = 1
+  OBSIDIAN = 2
+  GEODE = 3
+
+
+class Global(object):
+  total_branches = 0
+  cache = dict()
+  cache_hits = 0
+  cache_misses = 0
+
+  @staticmethod
+  def reset():
+    Global.total_branches = 0
+    Global.cache = dict()
+    Global.cache_hits = 0
+    Global.cache_misses = 0
+
+
+class Robot(object):
+  def __init__(self, type, cost):
+    self.type = type
+    self.cost = cost
+
+  def __repr__(self):
+    return f"Robot(type={self.type}, cost={self.format_cost()}"
+
+  def format_cost(self):
+    return " and ".join(f"{amount} {Resource(index).name.lower()}" for index, amount in enumerate(self.cost) if amount > 0)
+
+
+def buildable_robot_types(blueprint, collection):
+  # can choose to not build a robot
+  result = [None]
+  for resource in Resource:
+    can_build = True
+    for cost_resource_index, cost_value in enumerate(blueprint[resource].cost):
+      if collection[cost_resource_index] < cost_value:
+        can_build = False
+    if can_build:
+      result.append(resource)
+  return [Resource.GEODE] if Resource.GEODE in result else result
+
+
+def update_cache(robots, collection, minutes_remaining, choices, collected):
+  Global.cache[(robots, collection, minutes_remaining)] = (choices, collected)
+
+
+def read_cache(robots, collection, minutes_remaining):
+  key = (robots, collection, minutes_remaining)
+  if key in Global.cache:
+    return Global.cache[key]
+  return None
+
+
+def merge(collected1, collected2):
+  total = dict()
+  for resource in collected1.keys():
+    total[resource] = collected1.get(resource, 0) + collected2.get(resource, 0)
+  return total
+
+
+def greater_than(collected1, collected2):
+  for resource in reversed(Resource):
+    if collected1[resource.value] > collected2[resource.value]:
+      return True
+    elif collected1[resource.value] < collected2[resource.value]:
+      return False
+  return False
+
+
+def update_amounts(amounts, resource, value):
+  return tuple(amount + (value if index == resource.value else 0) for index, amount in enumerate(amounts))
+
+
+def add_amounts(amounts1, amounts2, factor=1):
+  return tuple(a1 + (a2 * factor) for a1, a2 in zip(amounts1, amounts2))
+
+
+def subtract_amounts(amounts1, amounts2):
+  return add_amounts(amounts1, amounts2, factor=-1)
+
+
+def run(blueprint, robots, collection, total_minutes, minutes_remaining):
+  if minutes_remaining == 0:
+    Global.total_branches += 1
+    if Global.total_branches % 1000000 == 0:
+      print(f"branches so far: {Global.total_branches}")
+    return [], collection
+  cache_result = read_cache(robots, collection, minutes_remaining)
+  if cache_result:
+    Global.cache_hits += 1
+    return cache_result
+  Global.cache_misses += 1
+  buildable_types = buildable_robot_types(blueprint, collection)
+  best_choices = []
+  best_collected = init_resource_amounts()
+  collection = add_amounts(collection, robots)
+  for build_resource_type in buildable_types:
+    if build_resource_type:
+      robots = update_amounts(robots, build_resource_type, 1)
+      collection = subtract_amounts(collection, blueprint[build_resource_type].cost)
+    choices, collected = run(blueprint, robots, collection, total_minutes, minutes_remaining - 1)
+    if greater_than(collected, best_collected):
+      best_choices = [build_resource_type] + choices
+      best_collected = collected
+  update_cache(robots, collection, minutes_remaining, best_choices, best_collected)
+  return best_choices, best_collected
+
+
+def init_resource_amounts():
+  return 0, 0, 0, 0
+
+
+def print_choices(choices, blueprint, minutes_remaining):
+  robots = [1, 0, 0, 0]
+  collection = [0, 0, 0, 0]
+  for build_robot_type, minute in zip(choices, range(minutes_remaining)):
+    print(f"\n== Minute {minute + 1} ==")
+    if build_robot_type:
+      for resource_type, amount in enumerate(blueprint[build_robot_type].cost):
+        collection[resource_type] -= amount
+      if build_robot_type == Resource.GEODE:
+        print(f"Spend {blueprint[Resource.GEODE].format_cost()} to start building a geode-cracking robot.")
+      else:
+        print(f"Spend {blueprint[build_robot_type].format_cost()} to start building a {build_robot_type.name.lower()}-collecting robot.")
+    for resource_type, n_robots in enumerate(robots):
+      if n_robots == 0:
+        continue
+      collection[resource_type] += n_robots
+      resource = Resource(resource_type)
+      if resource_type == Resource.GEODE:
+        print(f"{n_robots} geode-cracking robot{'s' if n_robots > 1 else ''} crack{'s' if n_robots == 1 else ''} geodes; you now have {collection[Resource.GEODE.value]} open geodes.")
+      else:
+        print(f"{n_robots} {resource.name.lower()}-collecting robot{'s' if n_robots > 1 else ''} collect{'s' if n_robots == 1 else ''} {resource.name.lower()}; you now have {collection[resource_type]} {resource.name.lower()}.")
+    if build_robot_type:
+      robots[build_robot_type.value] += 1
+      print(f"The new {build_robot_type.name.lower()}-collecting robot is ready; you now have {robots[build_robot_type.value]} of them.")
+
+
+def main(file, minutes):
+  with open(file, "r") as f:
+    Global.reset()
+    blueprint = dict()
+    for line in f:
+      if match := re.match(f"Blueprint (\d+):", line):
+        blueprint_id = int(match.group(1))
+      else:
+        raise Exception(f"invalid input line: {line}")
+      if matches := re.findall(r"(Each (\w+) robot costs (\d+) (\w+)( and (\d+) (\w+))?.)", line):
+        for match in matches:
+          robot_type = Resource[match[1].upper()]
+          cost = update_amounts(init_resource_amounts(), Resource[match[3].upper()], int(match[2]))
+          if len(match[4]) > 0:
+            cost = update_amounts(cost, Resource[match[6].upper()], int(match[5]))
+          blueprint[robot_type] = Robot(robot_type, cost)
+      else:
+        raise Exception(f"invalid input line: {line}")
+      print(f"\n\n*** Blueprint {blueprint_id} ***")
+      for resource in Resource:
+        print(f"  Each {resource.name.lower()} robot costs {blueprint[resource].format_cost()}.")
+      robots = (1, 0, 0, 0)
+      collection = init_resource_amounts()
+      choices, collected = run(blueprint, robots, collection, minutes, minutes)
+      print_choices(choices, blueprint, minutes)
+      print(f"\nchoices: {choices}")
+      print(f"final collection: {collected}")
+      print(f"cache hits: {Global.cache_hits}")
+      print(f"cache misses: {Global.cache_misses}")
+      print(f"total branches examined: {Global.total_branches}")
+      return
+
+
+if __name__ == "__main__":
+  parser = ArgumentParser()
+  parser.add_argument("file")
+  parser.add_argument("minutes", type=int)
+  args = parser.parse_args()
+  start_time = time.time()
+  main(args.file, args.minutes)
+  print("--- COMPLETED IN %s SECONDS ---" % (time.time() - start_time))
