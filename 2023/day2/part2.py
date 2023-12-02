6cf29d8d4224fde04079f2cdf13c9ab0d0f0bd9a
Patrick Stover <stoverp@colorado.edu>
2023-12-02 11:16:59 -0500

    day2 part2

---
+++ 2023/day2/part2.py
@@ -0,0 +1,39 @@
+import re
+import time
+from argparse import ArgumentParser
+from collections import defaultdict
+from functools import reduce
+
+def game(pulls):
+  max_per_color = defaultdict(int)
+  for pull in pulls:
+    for color_pull in re.split(r"\s*,\s*", pull):
+      num_str, color = color_pull.split(" ")
+      max_per_color[color] = max(max_per_color[color], int(num_str))
+  print(max_per_color)
+  return reduce(lambda x, y: x * y, max_per_color.values())
+
+
+def main(file):
+  total = 0
+  with open(file, "r") as f:
+    for line in f:
+      print(line.strip())
+      if match := re.match(r"Game (\d+): (.*)$", line.strip()):
+        game_num = int(match.group(1))
+        pulls = re.split(r"\s*;\s*", match.group(2))
+        power = game(pulls)
+        print(f"Game {game_num} power: {power}\n")
+        total += power
+      else:
+        raise Exception(f"invalid input line: {line}")
+  return total
+
+
+if __name__ == "__main__":
+  parser = ArgumentParser()
+  parser.add_argument("file")
+  args = parser.parse_args()
+  start_time = time.time()
+  print("\nRESULT:", main(args.file))
+  print("--- COMPLETED IN %s SECONDS ---" % (time.time() - start_time))
