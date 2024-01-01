c0fb7ac2c4817be178f4c442692aa1837e347e7c
Patrick Stover <stoverp@colorado.edu>
2024-01-01 13:35:38 -0500

    day11 part2

--- 2023/day11/part2.py
+++ 2023/day11/part2.py
@@ -3,21 +3,30 @@ import time
 from argparse import ArgumentParser
 
 
+EXPANSION_SIZE = 999999
+
+
 def read_input(file):
   sky = []
+  galaxies = []
+  row = 0
   with open(file, "r") as f:
     for line in f:
       sky.append(line.strip())
-  return sky
+      for col, c in enumerate(line.strip()):
+        if c == "#":
+          galaxies.append((row, col))
+      row += 1
+  return galaxies, sky
 
 
 def expand_rows(sky):
-  new_sky = []
-  for line in sky:
+  row_coords = [i for i in range(len(sky))]
+  for row, line in enumerate(sky):
     if "#" not in line:
-      new_sky.append(line)
-    new_sky.append(line)
-  return new_sky
+      for i in range(row + 1, len(row_coords)):
+        row_coords[i] += EXPANSION_SIZE
+  return row_coords
 
 
 def expand_cols(sky):
@@ -26,21 +35,11 @@ def expand_cols(sky):
     for col, c in enumerate(line):
       if c == "#" and col in empty_cols:
         empty_cols.remove(col)
-  new_sky = []
-  for line in sky:
-    new_line = ""
-    for col, c in enumerate(line):
-      if col in empty_cols:
-        new_line += c
-      new_line += c
-    new_sky.append(new_line)
-  return new_sky
-
-
-def print_sky(sky):
-  for line in sky:
-    print(line)
-  print()
+  col_coords = [i for i in range(len(sky[0]))]
+  for col in empty_cols:
+    for i in range(col + 1, len(col_coords)):
+      col_coords[i] += EXPANSION_SIZE
+  return col_coords
 
 
 def find_galaxies(sky):
@@ -60,15 +59,30 @@ def distances_sum(galaxies):
   return total
 
 
+def print_sky(sky):
+  for line in sky:
+    print(line)
+  print()
+
+
+def map_galaxies(galaxies, row_coords, col_coords):
+  mapped_galaxies = []
+  for galaxy in galaxies:
+    mapped_galaxies.append((row_coords[galaxy[0]], col_coords[galaxy[1]]))
+  return mapped_galaxies
+
+
 def main(file):
-  sky = read_input(file)
-  # print_sky(sky)
-  sky = expand_rows(sky)
-  sky = expand_cols(sky)
-  # print_sky(sky)
-  galaxies = find_galaxies(sky)
+  galaxies, sky = read_input(file)
   # print(galaxies)
-  return distances_sum(galaxies)
+  # print_sky(sky)
+  row_coords = expand_rows(sky)
+  # print(row_coords)
+  col_coords = expand_cols(sky)
+  # print(col_coords)
+  mapped_galaxies = map_galaxies(galaxies, row_coords, col_coords)
+  # print(mapped_galaxies)
+  return distances_sum(mapped_galaxies)
 
 
 if __name__ == "__main__":
