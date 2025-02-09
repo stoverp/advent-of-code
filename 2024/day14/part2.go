182e28cf8e8b12cb2949e427c6799d70a31b19cb
Patrick Stover <stoverp@colorado.edu>
2025-02-09 09:55:25 -0500

    day14 part2

--- 2024/day14/part2.go
+++ 2024/day14/part2.go
@@ -61,33 +61,67 @@ func wrap(v int64, limit int64) int64 {
 	}
 }
 
-func printRobots(robots []Robot, width int64, height int64) {
-	console := make([][]int64, height)
-	for i := range console {
-		console[i] = make([]int64, width)
-	}
-	for _, robot := range robots {
-		console[robot.py][robot.px] += 1
-	}
-	for y := range height {
-		for x := range width {
-			if console[y][x] == 0 {
-				fmt.Print(".")
-			} else {
-				fmt.Print(console[y][x])
+func hasNeighbor(console [][]int64, x int64, y int64, width int64, height int64) bool {
+	for dy := y - 1; dy <= y+1; dy++ {
+		for dx := x - 1; dx <= x+1; dx++ {
+			if dy < 0 || dx < 0 || dy >= height || dx >= width || (dy == y && dx == x) {
+				continue
+			}
+			if console[dy][dx] > 0 {
+				return true
 			}
 		}
-		fmt.Println()
 	}
+	return false
+}
+
+func candidate(console [][]int64, robots []Robot) bool {
+	noNeighborCount := 0
+	for _, robot := range robots {
+		if !hasNeighbor(console, robot.px, robot.py, int64(len(console[0])), int64(len(console))) {
+			noNeighborCount += 1
+		}
+		if noNeighborCount > 200 {
+			return false
+		}
+	}
+	fmt.Println("no neighbor count:", noNeighborCount)
+	return true
 }
 
 func run(filename string, width int64, height int64) int {
 	robots := read(filename)
 	reader := bufio.NewReader(os.Stdin)
+	console := make([][]int64, height)
+	for i := range console {
+		console[i] = make([]int64, width)
+	}
 	for second := range 100000000 {
-		fmt.Println("second:", second)
-		printRobots(robots, width, height)
-		_, _ = reader.ReadString('\n')
+		// clear console
+		for y := range height {
+			for x := range width {
+				console[y][x] = 0
+			}
+		}
+		// add robots
+		for _, robot := range robots {
+			console[robot.py][robot.px] += 1
+		}
+		// print console if it's a candidate
+		if candidate(console, robots) {
+			fmt.Println("second:", second)
+			for y := range height {
+				for x := range width {
+					if console[y][x] == 0 {
+						fmt.Print(".")
+					} else {
+						fmt.Print(console[y][x])
+					}
+				}
+				fmt.Println()
+			}
+			_, _ = reader.ReadString('\n')
+		}
 		for i := range robots {
 			robots[i].px = wrap(robots[i].px+robots[i].vx, width)
 			robots[i].py = wrap(robots[i].py+robots[i].vy, height)
